bd9a51acc7ef56d61c000603b08415f7400609d9
Viviane <vivianedelucenasilva@gmail.com>
2025-06-26 10:15:11 -0300

    aula poo

--- src/MetodosPOO/Calculadora.java
+++ src/MetodosPOO/Calculadora.java
@@ -1,7 +1,11 @@
 package MetodosPOO;
 
 public class Calculadora {
-	
+	public static void main(String[] args) {
+		CalcularMetodos num1 = new CalcularMetodos();
+		
+		num1.somar(50, 30)
+	}
 	
 	
 }
